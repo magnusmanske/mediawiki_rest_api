804df8efc9c5d4c8ec02f081266967c235c9dd9d
Magnus Manske <magnusmanske@googlemail.com>
2025-10-23 14:22:35 +0100

    all mock servers now

--- src/file.rs
+++ src/file.rs
@@ -33,11 +33,40 @@ impl File {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::rest_api_builder::RestApiBuilder;
+    use serde_json::Value;
+    use wiremock::matchers::{method, path};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    async fn get_mock_api(test_file: &str, test_path: &str) -> (RestApi, MockServer) {
+        let mock_path = format!("w/rest.php/v1{}", test_path.replace(' ', "%20"));
+        let mock_server = MockServer::start().await;
+
+        let test_text: String =
+            std::fs::read_to_string(format!("test_data/{test_file}")).expect("Test file missing");
+        if test_file.ends_with(".json") {
+            let json: Value = serde_json::from_str(&test_text).expect("Failed to parse JSON");
+            Mock::given(method("GET"))
+                .and(path(&mock_path))
+                .respond_with(ResponseTemplate::new(200).set_body_json(&json))
+                .mount(&mock_server)
+                .await;
+        } else {
+            Mock::given(method("GET"))
+                .and(path(&mock_path))
+                .respond_with(ResponseTemplate::new(200).set_body_string(&test_text))
+                .mount(&mock_server)
+                .await;
+        }
+
+        let api = RestApi::builder(&(mock_server.uri() + "/w/rest.php"))
+            .expect("Failed to create RestApi")
+            .build();
+        (api, mock_server)
+    }
 
     #[tokio::test]
     async fn test_get() {
-        let api = RestApiBuilder::wikipedia("en").build();
+        let (api, _mock_server) = get_mock_api("file_get.json", "/file/Commons-logo.svg").await;
         let page = File::new("Commons-logo.svg");
         let file_info = page.get(&api).await.expect("Failed to get page content");
         assert_eq!(
